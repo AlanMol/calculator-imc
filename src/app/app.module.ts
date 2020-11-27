d74fd38a0469eec518b211ad70324dad552df5c9
DannaLuisa <danna12.garo@gmail.com>
2020-11-26 22:42:18 -0600

    ve1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { UiComponent } from './ui/ui.component';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UiComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    MatFormFieldModule,
+   ,FormsModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
